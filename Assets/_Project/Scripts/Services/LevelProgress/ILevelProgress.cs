be2b118d6029994934052e81bd7fe860f1fd1858
Mikov Denis <45800215+TaggedDev@users.noreply.github.com>
2026-03-28 21:10:55 +0300

    Removed StaticData dependency from the LevelProgress
    Co-Authored-By: GalteXx <65533265+GalteXx@users.noreply.github.com>

--- Assets/_Project/Scripts/Services/LevelProgress/ILevelProgress.cs
+++ Assets/_Project/Scripts/Services/LevelProgress/ILevelProgress.cs
@@ -1,14 +1,7 @@
-using System;
-
 namespace _Project.CurrentLevelProgress
 {
     public interface ILevelProgress
     {
-        event EventHandler WaveCleared;
-        event EventHandler LevelCleared;
-        event EventHandler PlayerCoreDestroyed;
-        LevelConfig LoadedLevelConfig { get; }
-        bool IsLevelSuccessfullyFinished { get; }
         void LoadLevelConfig(LevelConfig levelConfig);
     }
 }
