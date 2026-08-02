256c5c4acf4e572be7170e9317f99c47a133ee21
Korsakov Maksim <65533265+GalteXx@users.noreply.github.com>
2026-08-02 23:53:43 +0200

    added Friend->PlayerModel translator for Steam Multiplayer

---
+++ Assets/_Project/Scripts/Services/Multiplayer/Players/Steam/PlayerModelTranslator.cs
@@ -0,0 +1,40 @@
+using System.Threading.Tasks;
+using Steamworks;
+using Steamworks.Data;
+using UnityEngine;
+using Color = UnityEngine.Color;
+
+namespace _Project.Multiplayer.Players.Steam.Steam
+{
+    public class PlayerModelTranslator
+    {
+        public async Task<PlayerModel> CreatePlayerModel(Friend friend)
+        {
+            var avatar = Texture2D.blackTexture;
+            if (await friend.GetLargeAvatarAsync() is { } image) avatar = GetPlayerAvatarTexture(image);
+            var playingSameGame = false;
+            if (friend.GameInfo is { } gameInfo)
+                playingSameGame = gameInfo.GameID == 480; // Inject game id? (Singleton it!!!!)
+
+            return new PlayerModel(avatar, friend.Id.ToString(), friend.Name, friend.Nickname, playingSameGame,
+                friend.State);
+        }
+
+        private static Texture2D GetPlayerAvatarTexture(Image image)
+        {
+            var avatar = new Texture2D((int)image.Width, (int)image.Height, TextureFormat.ARGB32, false);
+            avatar.filterMode = FilterMode.Trilinear;
+
+            for (var x = 0; x < image.Width; x++)
+            for (var y = 0; y < image.Height; y++)
+            {
+                var p = image.GetPixel(x, y);
+                avatar.SetPixel(x, (int)image.Height - y,
+                    new Color(p.r / 255.0f, p.g / 255.0f, p.b / 255.0f, p.a / 255.0f));
+            }
+
+            avatar.Apply();
+            return avatar;
+        }
+    }
+}
