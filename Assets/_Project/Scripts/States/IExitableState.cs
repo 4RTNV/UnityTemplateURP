05d339cbe8ccfe1476545abb43ee5b60ef71375d
Mikov Denis <45800215+TaggedDev@users.noreply.github.com>
2026-08-02 17:06:03 +0300

    Split the States into the 3 different files

---
+++ Assets/_Project/Scripts/States/IExitableState.cs
@@ -0,0 +1,7 @@
+﻿namespace _Project.States
+{
+    public interface IExitableState
+    {
+        void Exit();
+    }
+}
--- Assets/_Project/Scripts/States/IState.cs
+++ Assets/_Project/Scripts/States/IState.cs
@@ -1,17 +1,7 @@
 ﻿namespace _Project.States
 {
-    public interface IExitableState
-    {
-        void Exit();
-    }
-
     public interface IState : IExitableState
     {
         void Enter();
-    }
-
-    public interface IPayloadedState<TPayLoad> : IExitableState
-    {
-        void Enter(TPayLoad payload);
     }
 }
