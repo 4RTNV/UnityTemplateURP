80e60717eb91a6d37aefa80e2302bddda27e0979
Korsakov Maksim <65533265+GalteXx@users.noreply.github.com>
2026-08-17 20:13:32 +0200

    removed scoped dependencies from appState

--- Assets/_Project/Scripts/Services/StateMachines/AppStateMachines/LoadLevelState.cs
+++ Assets/_Project/Scripts/Services/StateMachines/AppStateMachines/LoadLevelState.cs
@@ -1,8 +1,5 @@
-﻿using _Project.CurrentLevelProgress;
-using _Project.Factory;
-using _Project.PersistentProgress;
+﻿using _Project.PersistentProgress;
 using _Project.StaticData;
-using _Project.UI.Factory;
 using UnityEngine;
 
 namespace _Project.StateMachines
@@ -10,27 +7,19 @@ namespace _Project.StateMachines
     public class LoadLevelState : IPayloadedAppState<string>
     {
         private readonly AppStateMachine _appStateMachine;
-        private readonly IGameFactory _gameFactory;
-        private readonly ILevelProgress _levelProgress;
         private readonly Canvas _loadingCurtain;
         private readonly IPersistentProgress _progress;
         private readonly IStaticData _staticData;
-        private readonly IUIFactory _uiFactory;
 
-        public LoadLevelState(AppStateMachine appStateMachine, IGameFactory gameFactory, IPersistentProgress progress,
-            IStaticData staticData, IUIFactory uiFactory, ILevelProgress levelProgress)
+        public LoadLevelState(AppStateMachine appStateMachine, IPersistentProgress progress, IStaticData staticData)
         {
             _appStateMachine = appStateMachine;
-            _gameFactory = gameFactory;
             _progress = progress;
             _staticData = staticData;
-            _uiFactory = uiFactory;
-            _levelProgress = levelProgress;
         }
 
         public void Enter(string sceneName)
         {
-            _gameFactory.CleanUp();
             _loadingCurtain.gameObject.SetActive(true);
         }
 
