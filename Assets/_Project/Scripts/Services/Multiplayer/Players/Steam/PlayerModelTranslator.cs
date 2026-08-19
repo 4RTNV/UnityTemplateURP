c75eab73378663620097297d5070bbb65a2b111b
Korsakov Maksim <65533265+GalteXx@users.noreply.github.com>
2026-08-19 23:28:28 +0200

    small translator fixes

--- Assets/_Project/Scripts/Services/Multiplayer/Players/Steam/PlayerModelTranslator.cs
+++ Assets/_Project/Scripts/Services/Multiplayer/Players/Steam/PlayerModelTranslator.cs
@@ -1,4 +1,3 @@
-using System.Threading.Tasks;
 using Steamworks;
 using Steamworks.Data;
 using UnityEngine;
@@ -6,9 +5,9 @@ using Color = UnityEngine.Color;
 
 namespace _Project.Multiplayer.Players.Steam
 {
-    internal class PlayerModelTranslator
+    internal static class PlayerModelTranslator
     {
-        public static async Task<PlayerModel> CreatePlayerModel(Friend friend)
+        public static async Awaitable<PlayerModel> CreatePlayerModel(Friend friend)
         {
             var avatar = Texture2D.blackTexture;
             if (await friend.GetLargeAvatarAsync() is { } image) avatar = GetPlayerAvatarTexture(image);
@@ -24,15 +23,17 @@ namespace _Project.Multiplayer.Players.Steam
         {
             var avatar = new Texture2D((int)image.Width, (int)image.Height, TextureFormat.ARGB32, false)
             {
-                filterMode = FilterMode.Trilinear
+                filterMode = FilterMode.Trilinear,
             };
 
             for (var x = 0; x < image.Width; x++)
-            for (var y = 0; y < image.Height; y++)
             {
-                var p = image.GetPixel(x, y);
-                avatar.SetPixel(x, (int)image.Height - y,
-                    new Color(p.r / 255.0f, p.g / 255.0f, p.b / 255.0f, p.a / 255.0f));
+                for (var y = 0; y < image.Height; y++)
+                {
+                    var p = image.GetPixel(x, y);
+                    avatar.SetPixel(x, (int)image.Height - y,
+                        new Color(p.r / 255.0f, p.g / 255.0f, p.b / 255.0f, p.a / 255.0f));
+                }
             }
 
             avatar.Apply();
