958c433c3239e1b71309dc9091edcbc75be48447
Korsakov Maksim <65533265+GalteXx@users.noreply.github.com>
2026-08-20 14:02:14 +0200

    reactivated FriendshipTester

--- Assets/_Project/Scripts/Services/Multiplayer/FriendshipTester.cs
+++ Assets/_Project/Scripts/Services/Multiplayer/FriendshipTester.cs
@@ -1,5 +1,5 @@
 using System.Collections.Generic;
-using System.Threading.Tasks;
+using System.Linq;
 using _Project.Multiplayer.Players;
 using Reflex.Attributes;
 using UnityEngine;
@@ -19,21 +19,26 @@ namespace _Project.Multiplayer
 
         private void Update()
         {
-            // _client?.Update();
+            _client?.Update();
         }
 
         [Inject]
-        public async Task Initialize()
+        public void Initialize(IMultiplayerClient client, IFriendsCatalog catalog)
         {
-            /*_client = client;
-            _catalog = friendsCatalog;
-            await foreach (var friend in friendsCatalog.GetFriendsAsync()) _friends.Add(friend);
+            _client = client;
+            _catalog = catalog;
+            _ = CreateFriendsList();
+        }
+
+        private async Awaitable CreateFriendsList()
+        {
+            await foreach (var friend in _catalog.GetFriendsAsync()) _friends.Add(friend);
             Debug.Log($"FriendshipTester initialized with {_friends.Count} friends");
             _catalog.FriendDetailsChanged += (sender, model) =>
             {
                 Debug.Log($"FriendshipTester: Friend details changed: {model}");
                 _friends = _friends.Select(f => f.Equals(model) ? model : f).ToList();
-            };*/
+            };
         }
     }
 }
